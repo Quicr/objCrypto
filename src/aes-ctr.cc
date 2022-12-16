f9fa415e3d1805eb0b60ee2e8f1fc4224b63c95c
Cullen Jennings <fluffy@iii.ca>
2022-12-16 10:55:05 +0800

    add in booringssl version of aes128-ctr

--- src/aes-ctr.cc
+++ src/aes-ctr.cc
@@ -1,7 +1,14 @@
 #include <cassert>
 #include <cstring>
 
+#if defined( OBJ_CRYPTO_USE_BORINGSSL )
+#include <openssl/cipher.h>
+#endif
+
+#if defined( __APPLE__ )
 #include <CommonCrypto/CommonCryptor.h>
+#endif
+
 #include <objCrypto/objCrypto.h>
 
 #include "aes-ctr.h"
@@ -9,6 +16,7 @@
 using namespace ObjCrypto;
 
 
+#if defined( __APPLE__ ) && !defined( OBJ_CRYPTO_USE_BORINGSSL )
 ObjCryptoErr ObjCrypto::aes128_ctr_encrypt(const std::vector<char>& plainText,
                                 const Key128& key,
                                 const IV& iv, 
@@ -61,8 +69,10 @@ ObjCryptoErr ObjCrypto::aes128_ctr_encrypt(const std::vector<char>& plainText,
   
   return ObjCryptoErr::None;
 }
+#endif
 
     
+#if defined( __APPLE__ ) && !defined( OBJ_CRYPTO_USE_BORINGSSL )
 ObjCryptoErr ObjCrypto::aes128_ctr_decrypt( const std::vector<uint8_t>& cipherText,
                                  const Key128& key,
                                  const IV& iv ,
@@ -113,3 +123,97 @@ ObjCryptoErr ObjCrypto::aes128_ctr_decrypt( const std::vector<uint8_t>& cipherTe
   
   return ObjCryptoErr::None;
 }
+#endif
+
+
+
+#if defined( OBJ_CRYPTO_USE_BORINGSSL )
+ObjCryptoErr ObjCrypto::aes128_ctr_encrypt(const std::vector<char>& plainText,
+                                           const Key128& key,
+                                           const IV& iv, 
+                                           std::vector<uint8_t>& cipherText)
+{
+  EVP_CIPHER_CTX *ctx;
+  
+  assert( sizeof( key ) == 128/8 );
+  assert( sizeof( iv ) == 128/8 );
+  assert( plainText.size() == cipherText.size() );
+  
+  ctx = EVP_CIPHER_CTX_new();
+  assert( ctx );
+  
+  int status;
+  status = EVP_EncryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, NULL, NULL);
+  assert( status == 1 );
+  
+  status = EVP_EncryptInit_ex(ctx, NULL, NULL,
+                              (const uint8_t*)key.data(),
+                              (const uint8_t*)iv.data());
+  assert( status == 1 );
+  
+  int moved=0;
+  int cipherTextLen=0;
+  status = EVP_EncryptUpdate(ctx,
+                             (uint8_t*)cipherText.data(), &moved,
+                             (const uint8_t *)plainText.data(), plainText.size());
+  assert( status == 1 );
+  cipherTextLen += moved;
+  
+  status = EVP_EncryptFinal_ex(ctx, (uint8_t *)&cipherText[cipherTextLen], &moved);
+  assert( status == 1 );
+  cipherTextLen += moved;
+  
+  assert( cipherTextLen == cipherText.size() );
+  
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+  
+  return ObjCryptoErr::None;
+}
+#endif
+
+
+#if defined( OBJ_CRYPTO_USE_BORINGSSL )
+ObjCryptoErr ObjCrypto::aes128_ctr_decrypt( const std::vector<uint8_t>& cipherText,
+                                 const Key128& key,
+                                 const IV& iv ,
+                                 std::vector<char>& plainText )
+{
+  EVP_CIPHER_CTX *ctx;
+
+  assert( sizeof( key ) == 128/8 );
+  assert( sizeof( iv ) == 128/8 );
+  assert( plainText.size() == cipherText.size() );
+  
+  ctx = EVP_CIPHER_CTX_new();
+  assert( ctx );
+  
+  int status;
+  status = EVP_DecryptInit_ex(ctx, EVP_aes_128_ctr(), NULL, NULL, NULL);
+  assert( status == 1 );
+  
+  status = EVP_DecryptInit_ex(ctx, NULL, NULL,
+                              (const uint8_t*)key.data(),
+                              (const uint8_t*)iv.data());
+  assert( status == 1 );
+  
+  int moved=0;
+  int plainTextLen=0;
+  status = EVP_DecryptUpdate(ctx,
+                             (uint8_t*)plainText.data(), &moved,
+                             (const uint8_t *)cipherText.data(), cipherText.size());
+  assert( status == 1 );
+  plainTextLen += moved;
+  
+  status = EVP_DecryptFinal_ex(ctx, (uint8_t *)&plainText[plainTextLen], &moved);
+  assert( status == 1 );
+  plainTextLen += moved;
+  
+  assert( plainTextLen == plainText.size() );
+  
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+  
+  return ObjCryptoErr::None;
+}
+#endif
