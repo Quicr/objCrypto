b1eb4d025e8ab86eaabaf6894342bd3acb64e400
Cullen Jennings <fluffy@iii.ca>
2022-12-27 15:18:21 -0700

    add test case for AES 256 GCM

--- test/testAesGcm.cc
+++ test/testAesGcm.cc
@@ -150,7 +150,7 @@ TEST_CASE("test 2 AES128 GCM IV Mode")  {
 }
 */
 
-TEST_CASE("test 3 AES 128 GCM Mode") {
+TEST_CASE("test 4 AES 128 GCM Mode") {
     ObjCryptoErr err;
 
     ObjCryptor cryptor;
@@ -187,10 +187,8 @@ TEST_CASE("test 3 AES 128 GCM Mode") {
 
     std::vector<uint8_t> cipherText(plainTextIn.size());
     std::vector<uint8_t> plainTextOut(plainTextIn.size());
-
  
     KeyInfo keyInfo(ObjCryptoAlg::AES_128_GCM_128, key128);
-
 
     std::vector<uint8_t> tag(128 / 8);
     assert(tag.size() == 128 / 8);
@@ -217,6 +215,95 @@ TEST_CASE("test 3 AES 128 GCM Mode") {
     printHex("plainTextIn  ", plainTextIn.data(), plainTextIn.size());
     printHex(" plainTextOut", plainTextOut.data(), plainTextOut.size());
     printHex("       key128", key128.data(), key128.size());
+    printHex("        nonce", nonce.data(), nonce.size());
+    printHex("tag          ", tag.data(), tag.size());
+    printHex(" correctTag  ", correctTag.data(), correctTag.size());
+    printHex("cipherText   ", cipherText.data(), cipherText.size());
+    printHex(" correctText ", correct.data(), correct.size());
+
+    CHECK(correct.size() == cipherText.size());
+    for (int i = 0; i < correct.size(); i++) {
+        CHECK(correct[i] == cipherText[i]);
+    }
+
+    CHECK(correctTag.size() == tag.size());
+    for (int i = 0; i < correctTag.size(); i++) {
+        CHECK(correctTag[i] == tag[i]);
+    }
+
+    CHECK(plainTextIn.size() == plainTextOut.size());
+    for (int i = 0; i < plainTextIn.size(); i++) {
+        CHECK(plainTextIn[i] == plainTextOut[i]);
+    }
+}
+
+
+TEST_CASE("test 16 AES 256 GCM Mode") {
+    ObjCryptoErr err;
+
+    ObjCryptor cryptor;
+    KeyID keyId = 1;
+
+    // Test Case 16 from GCM Spec 
+    Key256 key256 = {
+      0xFE,0xFF,0xE9,0x92,0x86,0x65,0x73,0x1C,0x6D,0x6A,0x8F,0x94,0x67,0x30,0x83,0x08,
+      0xFE,0xFF,0xE9,0x92,0x86,0x65,0x73,0x1C,0x6D,0x6A,0x8F,0x94,0x67,0x30,0x83,0x08
+    };
+    Nonce nonce = {
+      0xCA,0xFE,0xBA,0xBE,0xFA,0xCE,0xDB,0xAD,0xDE,0xCA,0xF8,0x88
+    };
+
+    std::vector<uint8_t> plainTextIn = {
+      0xD9,0x31,0x32,0x25,0xF8,0x84,0x06,0xE5,0xA5,0x59,0x09,0xC5,0xAF,0xF5,0x26,0x9A,
+      0x86,0xA7,0xA9,0x53,0x15,0x34,0xF7,0xDA,0x2E,0x4C,0x30,0x3D,0x8A,0x31,0x8A,0x72,
+      0x1C,0x3C,0x0C,0x95,0x95,0x68,0x09,0x53,0x2F,0xCF,0x0E,0x24,0x49,0xA6,0xB5,0x25,
+      0xB1,0x6A,0xED,0xF5,0xAA,0x0D,0xE6,0x57,0xBA,0x63,0x7B,0x39
+    };
+    std::vector<uint8_t> authData = {
+      0xFE,0xED,0xFA,0xCE,0xDE,0xAD,0xBE,0xEF,0xFE,0xED,0xFA,0xCE,0xDE,0xAD,0xBE,0xEF,
+      0xAB,0xAD,0xDA,0xD2
+    };
+    
+    std::vector<uint8_t> correct = {
+      0x52,0x2D,0xC1,0xF0,0x99,0x56,0x7D,0x07,0xF4,0x7F,0x37,0xA3,0x2A,0x84,0x42,0x7D,
+      0x64,0x3A,0x8C,0xDC,0xBF,0xE5,0xC0,0xC9,0x75,0x98,0xA2,0xBD,0x25,0x55,0xD1,0xAA,
+      0x8C,0xB0,0x8E,0x48,0x59,0x0D,0xBB,0x3D,0xA7,0xB0,0x8B,0x10,0x56,0x82,0x88,0x38,
+      0xC5,0xF6,0x1E,0x63,0x93,0xBA,0x7A,0x0A,0xBC,0xC9,0xF6,0x62
+    };
+    std::vector<uint8_t> correctTag = {
+      0x76,0xFC,0x6E,0xCE,0x0F,0x4E,0x17,0x68,0xCD,0xDF,0x88,0x53,0xBB,0x2D,0x55,0x1B
+    };
+
+    std::vector<uint8_t> cipherText(plainTextIn.size());
+    std::vector<uint8_t> plainTextOut(plainTextIn.size());
+ 
+    KeyInfo keyInfo(ObjCryptoAlg::AES_256_GCM_128, key256);
+
+    std::vector<uint8_t> tag(128 / 8);
+    assert(tag.size() == 128 / 8);
+
+    err = cryptor.addKey(keyId, keyInfo);
+    assert(err == ObjCryptoErr::None);
+
+    err = cryptor.seal(keyId, nonce, plainTextIn, authData, tag, cipherText);
+    assert(err == ObjCryptoErr::None);
+
+    SUBCASE("good tag" ) {
+      err = cryptor.unseal(keyId, nonce, cipherText, authData, tag, plainTextOut);
+      assert(err != ObjCryptoErr::DecryptAuthFail);
+      assert(err == ObjCryptoErr::None);
+    }
+    SUBCASE("bad tag" ) {
+      tag[tag.size()-1]++;  // break tag
+
+      err = cryptor.unseal(keyId, nonce, cipherText, authData, tag, plainTextOut);
+      assert(err == ObjCryptoErr::DecryptAuthFail);
+      return;
+    }
+    
+    printHex("plainTextIn  ", plainTextIn.data(), plainTextIn.size());
+    printHex(" plainTextOut", plainTextOut.data(), plainTextOut.size());
+    printHex("       key256", key256.data(), key256.size());
     printHex("        nonce", nonce.data(), nonce.size());
     printHex("tag          ", tag.data(), tag.size());
     printHex(" correctTag  ", correctTag.data(), correctTag.size());
