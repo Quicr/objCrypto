05d565995f398c3a2a5b09ab190590fb5292d925
Cullen Jennings <fluffy@iii.ca>
2023-01-01 17:05:18 -0700

    vhange version file name

---
+++ include/objCrypto/version.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Copyright (c) 2022 The ObjCrypto Project Authors
+// SPDX-License-Identifier: BSD-2-Clause
+
+#pragma once
+
+namespace ObjCrypto {
+
+static const float objCryptoVersion =
+  0.1f;
+
+}
