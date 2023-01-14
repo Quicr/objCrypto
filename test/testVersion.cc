90759416d731efd94546d9c6c0581405757929d7
Cullen Jennings <fluffy@iii.ca>
2023-01-14 15:54:37 -0700

    test span code

--- test/testVersion.cc
+++ test/testVersion.cc
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: Copyright (c) 2022 The ObjCrypto Project Authors
 // SPDX-License-Identifier: BSD-2-Clause
 
+#include <iostream>
+#include <span>
+#include <vector>
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 #include <objCrypto/objCrypto.h>
@@ -10,4 +14,16 @@ using namespace ObjCrypto;
 
 TEST_CASE("Test version info") {
   CHECK(ObjCryptor::version() == ObjCrypto::objCryptoVersion());
+}
+
+// int testSpan( const std::span<uint8_t> &a ) {
+//   return a.size_bytes();
+// }
+
+TEST_CASE("Test span") {
+  uint8_t data[]{1, 2, 3};
+
+  auto a = std::span{data};
+
+  // CHECK( testSpan( std::span( a )  ) = 3 );
 }
